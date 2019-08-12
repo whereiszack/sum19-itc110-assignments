d54878120edbbed10646b99f4b955b501a3b5015
whereiszack <zobertson@gmail.com>
2019-08-12 12:33:14 -0700

    a program that calculates the fibonacci sequence

---
+++ fib.py
@@ -0,0 +1,9 @@
+#fib.py
+#This Program calculates the Fibonacci Sequence for a given value.
+#Zack Robertson
+#8-12-19
+
+def main():
+    print("This Program calculates the Fibonacci Sequence for a given value.") #program description
+    n = int(input("Please enter an integer value: "))
+    
